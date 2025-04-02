e9013604c55e084b36c8aaadf14f924033e70ad5
ixWavey <synxi@outlook.dk>
2025-04-02 10:52:46 +0200

    Clean up

--- bookManager.py
+++ bookManager.py
@@ -2,16 +2,16 @@ from bookClass import Book
 
 class BookManager:
 
-    book_id = 1
+    book_id = 0
 
     def __init__(self, library):
         self.library = library
 
     def new_book(self, title, author, copies):
-        book = Book(self.book_id, title,author)
         self.book_id += 1
+        book = Book(self.book_id, title,author)
         self.library.add_book(book,int(copies))
-        return self.book_id - 1
+        return self.book_id
 
     def latest_book(self):
         return self.book_id
--- memberManager.py
+++ memberManager.py
@@ -2,13 +2,13 @@ from memberClass import Member
 
 class MemberManager:
 
-    member_id = 1
+    member_id = 0
 
     def __init__(self, library):
         self.library = library
 
     def new_member(self, name, borrowed_books):
-        member = Member(self.member_id, name, borrowed_books)
         self.member_id += 1
+        member = Member(self.member_id, name, borrowed_books)
         self.library.add_member(member)
-        return self.member_id - 1
+        return self.member_id
